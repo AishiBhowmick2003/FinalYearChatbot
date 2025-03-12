92c3b62c3ec150b072ba3616d7544d45682b701f
Aishi Bhowmick <144830242+AishiBhowmick2003@users.noreply.github.com>
2025-03-12 10:58:05 +0530

    main.py

--- FinalYearChatbot/backend/main.py
+++ FinalYearChatbot/backend/main.py
@@ -17,7 +17,7 @@ if not HUGGINGFACE_API_KEY:
 class QueryRequest(BaseModel):
     query: str
 
-DB_FAISS_PATH =r"C:\Users\bhowm\OneDrive\Documents\FinalYearChatbot\chatbot_model\db_faiss"  # Path to your FAISS index
+DB_FAISS_PATH =r"C:\Users\bhowm\OneDrive\Documents\FinalYearChatbot\chatbot_model\db_faiss" 
 embedding_model = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
 
 try:
